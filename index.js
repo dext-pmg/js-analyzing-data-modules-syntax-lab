b3ab42db17e6d837b67bf5c838006a48fa550bb0
dexterous dawati <dexterousdawati@gmail.com>
2026-01-18 23:45:37 +0300

    complete lab

--- index.js
+++ index.js
@@ -4,3 +4,13 @@
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
 };
+const datejs = require('datejs');
+
+function combineUsers(...args) {
+  const combinedObject = { users: [] };
+  args.forEach(array => {
+    combinedObject.users.push(...array);
+  });
+  combinedObject.merge_date = new Date().toString('M/d/yyyy');
+  return combinedObject;
+}
